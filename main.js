7dee6a19d9a0a25c303266addf1b38dde7d7f54a
Andrea Staskova <andrea@intiveo.com>
2021-03-18 15:29:04 +0100

    two buttons added

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-// tady je místo pro náš program
+/*
+how to add /remove /toggle class to an element:
+
+let header = document.querySelector("h1");
+header.classList.add("zluty");
+header.classList.remove("zluty");
+header.classList.toggle("zluty");
+*/
+
+let lionText = document.querySelector(".lev");
+
+function boldFont() {
+    lionText.classList.toggle("boldparagraph");
+}
+
+function getRed() {
+    lionText.classList.toggle("cerveny");
+}
+
+function getBig(event) {
+    let computedStyle = window.getComputedStyle(lionText)
+    let currSize = parseInt(computedStyle.fontSize);
+    
+    console.log(currSize);
+    console.log(computedStyle.fontSize)
+    console.log(event);
+    
+    lionText.style.fontSize = (currSize + 1) + "px";
+
+}
