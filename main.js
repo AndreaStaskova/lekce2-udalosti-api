a75c9c81841fcdc6e84c5b5663a363749559c887
Andrea Staskova <andrea@intiveo.com>
2021-03-18 21:19:27 +0100

    added audio controllers

--- main.js
+++ main.js
@@ -18,13 +18,44 @@ function getRed() {
 }
 
 function getBig(event) {
-    let computedStyle = window.getComputedStyle(lionText)
+    let computedStyle = window.getComputedStyle(lionText);
     let currSize = parseInt(computedStyle.fontSize);
+    //použití toho window.getComputedStyle jsem našla na Stacku, protože lionText.style.fontSize mi vracelo NaN, nevím proč
     
     console.log(currSize);
-    console.log(computedStyle.fontSize)
+    console.log(computedStyle.fontSize);
     console.log(event);
     
     lionText.style.fontSize = (currSize + 1) + "px";
 
+}
+
+let song = document.getElementById("song")
+// nevím jestli je správné, že si proměnné pojmenovávám stejně v JS i v HTML???
+
+function play() {
+    song.play();
+}
+
+function pause() {
+    song.pause();
+}
+
+function volume(event) {
+    let slider = document.getElementById("slider");
+    //nechápu, ale když chybí řádka 44, tak to taky funguje, jak to?
+    let input = slider.value;
+    if (input == "0") {
+        song.volume = 0;
+    } else if (input == "0.5") {
+        song.volume = 0.5;
+    } else {
+        song.volume = 1;
+    }
+
+}
+
+function rewind() {
+    song.play();
+    song.currentTime = 0;
 }
